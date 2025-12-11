70680bd1177c630589d17914512391b6635200b8
Rohini432 <lalam.rohini@accenture.com>
2025-12-11 07:30:11 +0000

    Create Django project, configure MongoDB, update octofit_tracker app, populate octofit_db, and add prompt files

---
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -0,0 +1,36 @@
+# Django settings for octofit_tracker project
+
+INSTALLED_APPS = [
+    'octofit_tracker',
+    'rest_framework',
+    'djongo',
+    'corsheaders',
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_METHODS = ['*']
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'octofit_db',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'localhost',
+            'port': 27017,
+            'username': '',
+            'password': '',
+            'authSource': '',
+        },
+    }
+}
+
+ROOT_URLCONF = 'octofit_tracker.urls'
